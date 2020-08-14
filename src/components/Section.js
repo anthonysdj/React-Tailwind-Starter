ace4ccb779da4b463769431dfc9f7b0ba7a6b050
tons <anthony_dejesus@worldvision.org.ph>
2020-08-15 03:02:46 +0800

    add more params to section comp

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
-const Section = ({ bg, mw, children }) => {
+const Section = ({ bg, bgColor, opacity, mw, gutter, children }) => {
     if (!mw) mw = 'lg';
+    if (!gutter || typeof gutter !== 'number') gutter = 10;
+    if (!bgColor) bgColor = 'transparent';
+    if (!opacity || typeof opacity !== 'number') opacity = 100;
+
+    let background = { backgroundImage: 'transparent' }
+
+    if (bg) {
+        background = {
+            backgroundImage: `url(${bg})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            backgroundPositionY: 'top'
+        }
+    }
 
     return (
-        <div className={`pt-20 pb-20 bg-${bg}`}>
-            <div className={`m-auto max-w-screen-${mw}`}>
+        <div style={background} className={`bg-opacity-${opacity} bg-${bgColor} pt-${gutter} pb-${gutter} px-5 bg-${bg}`}>
+            <div className={`mx-auto max-w-screen-${mw}`}>
                 {children}
             </div>
         </div>
